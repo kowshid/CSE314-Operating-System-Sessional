a1bab205e979fdeb71cedd2eabf130de3b939194
Ahsanul Kabir <32612334+kowshid@users.noreply.github.com>
2018-11-30 20:07:55 +0600

    commit 3

--- Assignment2/1505102.cpp
+++ Assignment2/1505102.cpp
@@ -49,13 +49,14 @@ void * vanilla(void * arg)
 
 		q1.push(0);
 
-		printf("\nVanilla produced\n");
-		printf("Items in queue %d\n", q1.size());
-
 		pthread_mutex_unlock(&q1Lock);
+
+		printf("\nVanilla produced\n");
+		printf("Items in queue 1: %d\n", q1.size());
+
 		sem_post(&q1Occupied);
 
-		sleep(rand()%4+1);
+		sleep(1);
 	}
 }
 
@@ -70,10 +71,11 @@ void * chocolate(void * arg)
 
 		q1.push(1);
 
-		printf("\nChocolate produced\n");
-		printf("Items in queue %d\n", q1.size());
-
 		pthread_mutex_unlock(&q1Lock);
+
+		printf("\nChocolate produced\n");
+		printf("Items in queue 1: %d\n", q1.size());
+
 		sem_post(&q1Occupied);
 
 		sleep(rand()%4+1);
@@ -98,9 +100,14 @@ void * decorator(void * arg)
 		{
 			sem_wait(&q2Empty);
 			pthread_mutex_lock(&q23Lock);
+
 			q2.push(cake);
-			printf("\ndecorated a vanilla cake\n");
+
 			pthread_mutex_unlock(&q23Lock);
+
+			printf("\nDecorated a vanilla cake\n");
+			printf("Items in queue 2: %d\n", q2.size());
+
 			sem_post(&q2Occupied);
 		}
 
@@ -108,33 +115,43 @@ void * decorator(void * arg)
 		{
 			sem_wait(&q3Empty);
 			pthread_mutex_lock(&q23Lock);
+
 			q3.push(cake);
-			printf("\ndecorated a chocolate cake\n");
+
 			pthread_mutex_unlock(&q23Lock);
+
+			printf("\ndecorated a chocolate cake\n");
+			printf("Items in queue 3: %d\n", q3.size());
+
 			sem_post(&q3Occupied);
 		}
 
 		//pthread_mutex_unlock(&lock);
 		sem_post(&q1Empty);
 
-		sleep(rand()%4+1);
+		sleep(1);
 	}
 }
 
 void * waiter1(void *arg)
 {
-	printf("Waiter 2 check\n");
+	printf("Waiter 1 check\n");
 
 	while(1)
 	{
 		sem_wait(&q3Occupied);
 		pthread_mutex_lock(&q23Lock);
+
 		q3.pop();
+
 		pthread_mutex_unlock(&q23Lock);
+
 		printf("\nWaiter 1 served a chocolate cake\n");
+		printf("Items in queue 3: %d\n", q3.size());
+
 		sem_post(&q3Empty);
 
-		sleep(rand()%4+1);
+		sleep(1);
 	}
 }
 
@@ -146,12 +163,17 @@ void * waiter2(void *arg)
 	{
 		sem_wait(&q2Occupied);
 		pthread_mutex_lock(&q23Lock);
+
 		q2.pop();
+
 		pthread_mutex_unlock(&q23Lock);
+
 		printf("\nWaiter 2 served a vanilla cake\n");
+		printf("Items in queue 2: %d\n", q2.size());
+
 		sem_post(&q2Empty);
 
-		sleep(rand()%4+1);
+		sleep(1);
 	}
 }
 
